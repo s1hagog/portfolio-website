4c50045727d0ceee50f24f67d843bdbfbfbca812
s1hagog <moshakalex@gmail.com>
2020-04-16 16:19:28 -0700

    cursor styling and text

--- app.js
+++ app.js
@@ -10,17 +10,19 @@ function animateSlides() {
     const slides = document.querySelectorAll('.slide');
     const nav = document.querySelector('.nav-header');
 
-    slides.forEach( (slide, index, slides) => {
+    slides.forEach((slide, index, slides) => {
         const revealImg = slide.querySelector('.reveal-img');
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
 
         //GSAP
-        const slideTimeline = gsap.timeline({defaults: {duration: 1, ease: 'power2.inOut'}})
+        const slideTimeline = gsap.timeline({
+            defaults: {duration: 1, ease: 'power2.inOut'},
+        });
         slideTimeline.fromTo(revealImg, {x: '0%'}, {x: '100%'});
-        slideTimeline.fromTo(img, {scale:2}, {scale:1}, '-=1');
+        slideTimeline.fromTo(img, {scale: 2}, {scale: 1}, '-=1');
         slideTimeline.fromTo(revealText, {x: '0%'}, {x: '100%'}, '-=0.75');
-        slideTimeline.fromTo(nav, {y: '-100%'}, {y: '0%'}, '-=0.5')
+        slideTimeline.fromTo(nav, {y: '-100%'}, {y: '0%'}, '-=0.5');
 
         //Create scene
         slideScene = new ScrollMagic.Scene({
@@ -28,51 +30,75 @@ function animateSlides() {
             triggerHook: 0.25,
             reverse: false,
         })
-        .setTween(slideTimeline)
-        .addIndicators({colorStart: 'white', colorTrigger: 'white', name: 'slide'})
-        .addTo(controller)
-
+            .setTween(slideTimeline)
+            .addIndicators({
+                colorStart: 'white',
+                colorTrigger: 'white',
+                name: 'slide',
+            })
+            .addTo(controller);
 
         //New ANimatiomn
         const pageTimeline = gsap.timeline();
 
-        let nextSlide = slides.length - 1 === index ? 'end' : slides[index+1];
-        pageTimeline.fromTo(nextSlide, {y: '0%'}, {y: '50%'})
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+        pageTimeline.fromTo(nextSlide, {y: '0%'}, {y: '50%'});
 
-        pageTimeline.fromTo(slide, {opacity: 1, scale: 1}, {opacity: 0, scale: 0.5})
+        pageTimeline.fromTo(
+            slide,
+            {opacity: 1, scale: 1},
+            {opacity: 0, scale: 0.5}
+        );
 
-        pageTimeline.fromTo(nextSlide, {y: '50%'}, {y: '0%'}, '-=0.5')
+        pageTimeline.fromTo(nextSlide, {y: '50%'}, {y: '0%'}, '-=0.5');
 
         //Create New scene
         pageScene = new ScrollMagic.Scene({
             triggerElement: slide,
             duration: '100%',
-            triggerHook: 0
+            triggerHook: 0,
         })
-        .setPin(slide, {pushFollowers: false})
-        .setTween(pageTimeline)
-        .addIndicators({colorStart: 'white', colorTrigger: 'white', name: 'page', indent: 200})
-        .addTo(controller)
-    } )
+            .setPin(slide, {pushFollowers: false})
+            .setTween(pageTimeline)
+            .addIndicators({
+                colorStart: 'white',
+                colorTrigger: 'white',
+                name: 'page',
+                indent: 200,
+            })
+            .addTo(controller);
+    });
 }
 
 let mouse = document.querySelector('.cursor');
-function cursor(e){
-    mouse.style.top = e.pageY + 'px'
-    mouse.style.left = e.pageX + 'px'
+let mouseTxt = mouse.querySelector('span');
+
+function cursor(e) {
+    mouse.style.top = e.clientY + 'px';
+    mouse.style.left = e.clientX + 'px';
 }
 
-function activeCursor(e){
+function activeCursor(e) {
     const item = e.target;
 
-    if(item.id === 'logo' || item.classList.contains('burger')){
+    if (item.id === 'logo' || item.classList.contains('burger')) {
         mouse.classList.add('nav-active');
-    }else {
-        mouse.classList.remove('nav-active')
+    } else {
+        mouse.classList.remove('nav-active');
+    }
+
+    if (item.classList.contains('explore')) {
+        mouse.classList.add('explore-active');
+        mouseTxt.innerText = 'Tap';
+        gsap.to('.title-swipe', 1, {y: '0%'});
+    } else {
+        mouse.classList.remove('explore-active');
+        mouseTxt.innerText = 'Tap';
+        gsap.to('.title-swipe', 1, {y: '100%'});
     }
 }
 
-window.addEventListener('mousemove', cursor)
+window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 
 animateSlides();
