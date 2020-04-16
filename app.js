176087361e784b410629daed3c47f76236a27c62
s1hagog <moshakalex@gmail.com>
2020-04-15 23:25:18 -0700

    active cursor

--- app.js
+++ app.js
@@ -56,4 +56,23 @@ function animateSlides() {
     } )
 }
 
+let mouse = document.querySelector('.cursor');
+function cursor(e){
+    mouse.style.top = e.pageY + 'px'
+    mouse.style.left = e.pageX + 'px'
+}
+
+function activeCursor(e){
+    const item = e.target;
+
+    if(item.id === 'logo' || item.classList.contains('burger')){
+        mouse.classList.add('nav-active');
+    }else {
+        mouse.classList.remove('nav-active')
+    }
+}
+
+window.addEventListener('mousemove', cursor)
+window.addEventListener('mouseover', activeCursor);
+
 animateSlides();
