0c75c7f2dbdc7a37d0733e12cc51a90941ea56a5
s1hagog <moshakalex@gmail.com>
2020-04-17 10:54:02 -0700

    menu animated

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ function animateSlides() {
 
 let mouse = document.querySelector('.cursor');
 let mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
 
 function cursor(e) {
     mouse.style.top = e.clientY + 'px';
@@ -98,6 +99,25 @@ function activeCursor(e) {
     }
 }
 
+function navToggle(e) {
+    if (e.target.classList.toggle('active')) {
+        gsap.to('.line1', 0.5, {rotate: '45', y: 5, background: 'black'});
+        gsap.to('.line2', 0.5, {rotate: '-45', y: -5, background: 'black'});
+        gsap.to('#logo', 1, {color: 'black'});
+        gsap.to('.nav-bar', 1, {clipPath: 'circle(2500px at 100% -10%)'});
+        document.body.classList.add('hide');
+    } else {
+        gsap.to('.line1', 0.5, {rotate: '0', y: 0, background: 'white'});
+        gsap.to('.line2', 0.5, {rotate: '0', y: 0, background: 'white'});
+        gsap.to('#logo', 1, {color: 'white'});
+        gsap.to('.nav-bar', 1, {clipPath: 'circle(50px at 100% -10%)'});
+        document.body.classList.remove('hide');
+    }
+}
+
+// Event Listenres
+
+burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 
