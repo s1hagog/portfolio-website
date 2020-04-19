dbca8c01268aaf3e0f486589457a2fc0fc448860
s1hagog <moshakalex@gmail.com>
2020-04-18 17:31:12 -0700

    page transitions

--- app.js
+++ app.js
@@ -115,10 +115,79 @@ function navToggle(e) {
     }
 }
 
-// Event Listenres
+//Barba page transition
+const logo = document.querySelector('#logo');
+barba.init({
+    views: [
+        {
+            namespace: 'home',
+            beforeEnter() {
+                animateSlides();
+                logo.href = './index.html';
+            },
+            beforeLeave() {
+                slideScene.destroy();
+                pageScene.destroy();
+                controller.destroy();
+            },
+        },
+        {
+            namespace: 'fashion',
+            beforeEnter() {
+                logo.href = '../index.html';
+                gsap.fromTo(
+                    '.nav-header',
+                    1,
+                    {y: '100%'},
+                    {y: '0%', ease: 'power2.inOut'}
+                );
+            },
+        },
+    ],
+    transitions: [
+        {
+            leave({current, next}) {
+                let done = this.async();
 
+                //An Animation
+                const tl = gsap.timeline({defaults: {ease: 'power2.inOut'}});
+                tl.fromTo(current.container, 1, {opacity: 1}, {opacity: 0});
+                tl.fromTo(
+                    '.swipe',
+                    0.75,
+                    {x: '-100%'},
+                    {x: '0%', onComplete: done}
+                );
+                // tl.fromTo(
+                //     '.swipe',
+                //     0.75,
+                //     {x: '-100%'},
+                //     {x: '0%', stagger: 0.25, onComplete: done}
+                // );
+            },
+            enter({current, next}) {
+                let done = this.async();
+
+                //Scroll to the top
+                window.scrollTo(0, 0);
+
+                //An Animation
+                const tl = gsap.timeline({defaults: {ease: 'power2.inOut'}});
+                tl.fromTo(
+                    '.swipe',
+                    0.75,
+                    {x: '0%'},
+                    {x: '100%', stagger: 0.25, onComplete: done}
+                );
+                tl.fromTo(next.container, 1, {opacity: 0}, {opacity: 1});
+            },
+        },
+    ],
+});
+
+// Event Listenres
 burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 
-animateSlides();
+// animateSlides();
